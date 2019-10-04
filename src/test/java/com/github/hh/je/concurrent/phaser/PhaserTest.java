f73ff130b92069d0e105a69a64fd0fb13dc8ad4e
haihan yin <haihan.yin@rabobank.nl>
2019-10-04 13:28:58 +0200

    add two tests

--- src/test/java/com/github/hh/je/concurrent/phaser/PhaserTest.java
+++ src/test/java/com/github/hh/je/concurrent/phaser/PhaserTest.java
@@ -1,4 +1,37 @@
 package com.github.hh.je.concurrent.phaser;
 
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.TimeUnit;
+
 public class PhaserTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        Phaser phaser = new Phaser(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        executorService.submit(createRunnable(phaser));
+        executorService.submit(createRunnable(phaser));
+        executorService.submit(createRunnable(phaser));
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+    }
+
+    private Runnable createRunnable(Phaser phaser) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + ": phase1");
+                phaser.arriveAndAwaitAdvance();
+                System.out.println(Thread.currentThread().getName() + ": phase2");
+                phaser.arriveAndAwaitAdvance();
+                System.out.println(Thread.currentThread().getName() + ": phase3");
+            }
+        };
+    }
 }
--- src/test/java/com/github/hh/je/concurrent/semaphor/SemephorTest.java
+++ src/test/java/com/github/hh/je/concurrent/semaphor/SemephorTest.java
@@ -1,4 +1,46 @@
 package com.github.hh.je.concurrent.semaphor;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Semaphore;
+
 public class SemephorTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        Semaphore semaphore = new Semaphore(1);
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Callable<Object>> callableList = new ArrayList(20);
+        for (int i = 0; i<20; i++) {
+            callableList.add(createRunnable(semaphore));
+        }
+        executorService.invokeAll(callableList);
+    }
+
+    private static Callable createRunnable(Semaphore semaphore) {
+        return new Callable() {
+
+            @Override
+            public Object call() throws Exception {
+                try {
+                    semaphore.acquire();
+                    System.out.println("waiting queue: " + semaphore.getQueueLength());
+                    System.out.println(Thread.currentThread().getName() + " is executing...");
+                    Thread.sleep(300L);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+        };
+    }
 }
